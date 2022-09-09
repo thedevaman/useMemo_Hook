4a92c04b9162be39e407051b5d8c5a2a5f954b7a
thedevaman <devaman2508@gmail.com>
2022-09-09 09:16:38 +0530

    useMemo hook used

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useMemo } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+  const [item,setItem] = useState(10);
+
+ const multiCountMemo =useMemo(function multicount()
+  {
+    console.warn("multicount")
+    return count*5
+  },[count])   //only count is called then only multicount run
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useMemo Hooks</h1>
+      <h2>Count:{count}</h2>
+      <h2>Item:{item}</h2>
+      <h2>{multiCountMemo}</h2>
+      <button onClick={()=>setCount(count+1)}>Update Count</button>
+      <button onClick={()=>setItem(item*10)}>Update Item</button>
     </div>
   );
 }
